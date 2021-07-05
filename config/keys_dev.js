02e4803ad56e49fe714db030c3ca1c14ec5a6c30
FaridaElOraby <41236592+FaridaElOraby@users.noreply.github.com>
2021-07-05 10:59:20 +0200

    config prod

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://dbUser:1234@cluster0-shard-00-00.sxbfs.mongodb.net:27017,cluster0-shard-00-01.sxbfs.mongodb.net:27017,cluster0-shard-00-02.sxbfs.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-v4uk1u-shard-0&authSource=admin&retryWrites=true&w=majority",
-  salt: parseInt(10),
-  signingKey: "acl-project",
-};
-
-///config/keys_dev.js
